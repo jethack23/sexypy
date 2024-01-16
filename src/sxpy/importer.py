554e09682f05ca5271f87d31f7b07456dc375b4c
Jethack <jethack23@gmail.com>
2024-01-16 19:52:09 +0900

    [feat] import directory with __init__.ext and .py extension support

--- src/sxpy/importer.py
+++ src/sxpy/importer.py
@@ -13,25 +13,30 @@ class SyFinder(importlib.abc.MetaPathFinder):
     def find_spec(self, fullname, path=None, target=None):
         if path is None:
             path = []
+        else:
+            path = [path]
 
         path_extension = [os.getcwd()] + sys.path
         path += path_extension
 
         for entry in path:
-            filename = entry + "/" + fullname.replace(".", "/") + ".sy"
-            try:
-                with open(filename, "r"):
-                    return importlib.util.spec_from_file_location(
-                        fullname, filename, loader=SyLoader()
-                    )
-            except FileNotFoundError:
-                pass
+            for ext in [".sy", ".py", "/__init__.sy", "/__init__.py"]:
+                filename = entry + "/" + fullname.replace(".", "/") + ext
+                try:
+                    with open(filename, "r"):
+                        return importlib.util.spec_from_file_location(
+                            fullname, filename, loader=SyLoader()
+                        )
+                except FileNotFoundError:
+                    pass
 
         return None
 
 
 class SyLoader(importlib.abc.Loader):
     def exec_module(self, module):
+        if os.path.splitext(module.__file__)[0].endswith("__init__"):
+            module.__path__ = os.path.dirname(module.__file__)
         code = self.get_code_from_file(module.__file__)
         exec(code, module.__dict__)
 
@@ -45,10 +50,10 @@ class SyLoader(importlib.abc.Loader):
         if code is None:
             with open(filename, "rb") as file:
                 source = file.read().decode("utf-8")
-
-            parsed = parse(source)
-            ast_module = ast.Module(macroexpand_then_compile(parsed), type_ignores=[])
-            code = compile(ast_module, filename, "exec")
+            if filename.endswith(".sy"):
+                parsed = parse(source)
+                source = ast.Module(macroexpand_then_compile(parsed), type_ignores=[])
+            code = compile(source, filename, "exec")
         return code
 
     def get_code(self, fullname):
@@ -56,10 +61,11 @@ class SyLoader(importlib.abc.Loader):
         path_extension = [os.getcwd()] + sys.path
         path += path_extension
         for entry in path:
-            filename = entry + "/" + fullname.replace(".", "/") + ".sy"
-            try:
-                return self.get_code_from_file(filename)
-            except FileNotFoundError:
-                pass
+            for ext in [".sy", ".py"]:  # sy first
+                filename = entry + "/" + fullname.replace(".", "/") + ext
+                try:
+                    return self.get_code_from_file(filename)
+                except FileNotFoundError:
+                    pass
 
         return None
