2d3b74bec10c8ab4a0ae18511efb51f3ab61598e
Jethack <jethack23@gmail.com>
2024-07-07 20:57:16 +0900

    [feat] macro definition and require occurs python level.
    formerly, it happens compiler level, so after cached,
    something weird happened.

--- src/sxpy/core/macro.py
+++ src/sxpy/core/macro.py
@@ -4,49 +4,41 @@ from typing import Callable, Dict
 
 from sxpy.core.compiler import def_args_parse, stmt_list_compile
 from sxpy.core.nodes import *
+from sxpy.core.meta_functions import defmacro_transform, require_transform
 
 __macro_namespace: Dict[str, Callable[[Node], ast.AST]] = {}
 
 
-def define_macro(sexp, scope):
-    [op, macroname, args, *body] = sexp.list
-    new_name = "___macro___" + str(macroname)
-    def_exp = ast.FunctionDef(
-        name=new_name,
-        args=def_args_parse(args),
-        body=macroexpand_then_compile(body, scope),
-        decorator_list=[],
-        returns=None,
-        **sexp.position_info,
-    )
+def define_macro(sexp, scope, include_meta=True):
+    transformed = defmacro_transform(sexp)
     eval(
-        compile(ast.Interactive(body=[def_exp]), "macro-defining", "single"),
+        compile(
+            ast.Interactive(body=macroexpand_then_compile([transformed])),
+            "macro-defining",
+            "single",
+        ),
         scope,
     )
-    scope["__macro_namespace"][str(macroname)] = scope[new_name]
+    return transformed if include_meta else None
 
 
-def require_macro(sexp, scope):
-    [op, module_name, *optional] = sexp.list
-    imported_macros = importlib.import_module(
-        str(module_name).replace("-", "_")
-    ).__macro_namespace
-    if optional:
-        [macro_names] = optional
-        if str(macro_names) == "*":
-            scope["__macro_namespace"].update(imported_macros)
-        else:
-            for mac_name in map(str, macro_names.list):
-                scope["__macro_namespace"][mac_name] = imported_macros[mac_name]
-    else:
-        for k, v in imported_macros.items():
-            scope["__macro_namespace"][str(module_name) + "." + k] = v
+def require_macro(sexp, scope, include_meta=True):
+    transformed = require_transform(sexp)
+    eval(
+        compile(
+            ast.Interactive(body=macroexpand_then_compile([transformed])),
+            "macro-requiring",
+            "single",
+        ),
+        scope,
+    )
+    return transformed if include_meta else None
 
 
-def macroexpand(sexp, scope=globals()):
+def macroexpand(sexp, scope=globals(), include_meta=True):
     expanded = True
     while expanded:
-        sexp, expanded = macroexpand_1_and_check(sexp, scope)
+        sexp, expanded = macroexpand_1_and_check(sexp, scope, include_meta=include_meta)
     return sexp
 
 
@@ -54,7 +46,7 @@ def macroexpand_1(sexp, scope=globals()):
     return macroexpand_1_and_check(sexp, scope)[0]
 
 
-def macroexpand_1_and_check(sexp, scope=globals(), in_quasi=False):
+def macroexpand_1_and_check(sexp, scope=globals(), in_quasi=False, include_meta=True):
     expanded = False
     if isinstance(sexp, QuasiQuote):
         sexp.value, expanded = macroexpand_1_and_check(sexp.value, scope, in_quasi=True)
@@ -69,10 +61,10 @@ def macroexpand_1_and_check(sexp, scope=globals(), in_quasi=False):
     elif isinstance(sexp, Expression) and len(sexp) > 0:
         [op, *operands] = sexp.list
         if str(op) == "defmacro" and not in_quasi:
-            sexp = define_macro(sexp, scope)
+            sexp = define_macro(sexp, scope, include_meta=include_meta)
         elif str(op) == "require" and not in_quasi:
-            sexp = require_macro(sexp, scope)
-        elif str(op) in scope["__macro_namespace"] and not in_quasi:
+            sexp = require_macro(sexp, scope, include_meta=include_meta)
+        elif str(op) in scope.setdefault("__macro_namespace", {}) and not in_quasi:
             sexp = scope["__macro_namespace"][str(op)](*operands)
             expanded = True
         else:
@@ -87,14 +79,14 @@ def macroexpand_1_and_check(sexp, scope=globals(), in_quasi=False):
     return sexp, expanded
 
 
-def sexp_list_expand(sexp_list, scope):
+def sexp_list_expand(sexp_list, scope, include_meta=True):
     return filter(
-        lambda x: not x is None, map(lambda x: macroexpand(x, scope), sexp_list)
+        lambda x: not x is None,
+        map(lambda x: macroexpand(x, scope, include_meta=include_meta), sexp_list),
     )
 
 
-def macroexpand_then_compile(sexp_list, scope=globals()):
-    for k, v in globals().items():
-        if not k in scope:
-            scope[k] = v
-    return stmt_list_compile(sexp_list_expand(sexp_list, scope))
+def macroexpand_then_compile(sexp_list, scope=globals(), include_meta=True):
+    return stmt_list_compile(
+        sexp_list_expand(sexp_list, scope, include_meta=include_meta)
+    )
