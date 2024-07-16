8a7f92debe6ce5f5da41e6f0fc91899c7b5781da
Jethack <jethack23@gmail.com>
2024-07-16 20:00:21 +0900

    [feat] 0b, 0x, 0o notation for numbers

--- src/sxpy/core/compiler/literal.py
+++ src/sxpy/core/compiler/literal.py
@@ -14,6 +14,19 @@ def string_compile(string):
 
 def name_compile(symbol, ctx):
     [name, *attrs] = symbol.name.replace("-", "_").split(".")
+    # it's not name. it's a number but not captured with nubmer regex
+    if name.startswith("0x"):
+        assert ctx == ast.Load
+        symbol.value = str(int(name, 16))
+        return constant_compile(symbol)
+    elif name.startswith("0o"):
+        assert ctx == ast.Load
+        symbol.value = str(int(name, 8))
+        return constant_compile(symbol)
+    elif name.startswith("0b"):
+        assert ctx == ast.Load
+        symbol.value = str(int(name, 2))
+        return constant_compile(symbol)
     position_info = {**symbol.position_info}
     position_info["end_col_offset"] = position_info["col_offset"] + len(name)
     rst = ast.Name(id=name, ctx=ast.Load(), **position_info)
